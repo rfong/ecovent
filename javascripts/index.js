91350aee3727f5d996dce13cf997901add4c754b
rfong <rachefo@gmail.com>
2014-02-04 10:56:58 -0500

    pointed image paths to gh-pages blob

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,11 +1,12 @@
+var image_base_path = "https://github.com/rfong/ecovent/blob/gh-pages/images/";
 var slides = [
-  { image: "images/dip.jpg",
+  { image: "dip.jpg",
     caption: "Make every room in your home the right temperature."
   },
-  { image: "images/ecovent.jpg",
+  { image: "ecovent.jpg",
     caption: "Vents install in minutes, improving comfort immediately."
   },
-  { image: "images/lightbulb.jpg",
+  { image: "lightbulb.jpg",
     caption: "Put a stop to wasting money heating and cooling empty space."
   }
 ];
@@ -24,7 +25,7 @@ $(document).ready(function(){
   $.each(slides, function(i,slide) {
     $('.carousel-inner').append(
       Mustache.render(carousel_item_template, { 
-        image: slide.image,
+        image: image_base_path + slide.image,
         caption: slide.caption
         })
     );
